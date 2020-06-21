cd1cf7ba17df3a7e9debabd8c9c1a0828acc57e2
aakashsr <aakashsri.dev@gmail.com>
2020-06-21 13:05:20 +0530

    implemented attack functionality

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 new Vue({
     el: '#app',
     data: {
-        playerHealth: 10,
+        playerHealth: 100,
         monsterHealth: 100,
         isRunning: false
     },
     methods: {
-        toggleGame: function () {
+        startGame: function () {
             this.isRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+
+        attack: function () {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()) {
+                return;
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 10);
+            this.checkWin();
+        },
+
+        calculateDamage: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
+        },
+
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm("You won! New Game?")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+            } else if (this.playerHealth <= 0) {
+                if (confirm("You lost! New Game?")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+            }
+        },
+
+        specialAttack: function () {
+
+        },
+
+        heal: function () {
+
+        },
+
+        giveUp: function () {
+
         }
     }
 })
