39268854684c3a88940d03e0f33025e48e40a249
aakashsr <aakashsri.dev@gmail.com>
2020-06-21 11:45:38 +0530

    added styles to healthbar to adjust it's  as per data

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 new Vue({
     el: '#app',
     data: {
-        playerHealth: 100,
-        monsterHealth: 100
+        playerHealth: 10,
+        monsterHealth: 100,
+        isRunning: false
     }
 })
